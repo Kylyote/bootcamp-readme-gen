5fe0a259e3ed5cb7fdf56437362c2b3896e198a3
Kyle Jocoy <kjocoy@gmail.com>
2023-08-16 18:43:52 -0700

    added editer and the license image at the top of the read me

---
+++ README.md
@@ -0,0 +1,135 @@
+# alskdf
+
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
+## Description
+
+Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: 
+
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributors](#contributors)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: 
+
+## Usage
+
+Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: 
+
+## License
+
+This project is covered under the Mozilla Public License 2.0.
+
+## Contributors
+
+Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: 
+
+
+## Tests
+
+Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+
+In this example there will be no match for x: 
+
+## Questions
+
+For questions you can contact me at my [GitHub account](https://github.com/asdkfasdf).
+I can also be emailed at [alksjdfasd](alksjdfasd).
+
+### [Back to Top](#Description)
--- index.js
+++ index.js
@@ -5,48 +5,51 @@ const fs = require('fs');
 // Variables for tracking user options
 
 // README generation file that will have responses to prompts inserted into it
-function generateReadMe(title, description, installation, usage, license, contributing, tests, github, questionEmail) {
-    return `#${title}
+function generateReadMe(title, description, installation, usage, license, contributing, tests, github, questionEmail,licenseImage){
+return `# ${title}
 
-    ##Description:
-    ============
-    ${description}
-    
-    ${license}
+${licenseImage}
 
-    ##Table of Contents
-    =================
-     - [Installation](#installation)
-     - [Usage](#usage)
-     - [License](#license)
-     - [Contributors](#contributors)
-     - [Tests](#tests)
-     - [Questions](#questions)
-     
-    ##Installation
-    ============
-    ${installation}
-    
-    ##Usage
-    =====
-    ${usage}
-    
-    ##License
-    =======
-    This project is covered under the ${license}.
-    
-    ##Contributors
-    ============
-    ${contributing}
-    
-    ##Tests
-    =====
-    ${tests}
-    
-    ##Questions
-    =========
-    For questions you can contact me at my [GitHub account](https://github.com/${github}).
-    I can also be emailed at [${questionEmail}](${questionEmail}).`
+## Description
+
+${description}
+
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributors](#contributors)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## License
+
+This project is covered under the ${license}.
+
+## Contributors
+
+${contributing}
+
+## Tests
+
+${tests}
+
+## Questions
+
+For questions you can contact me at my [GitHub account](https://github.com/${github}).
+I can also be emailed at [${questionEmail}](${questionEmail}).
+
+### [Back to Top](#Description)`
 }
 
 // Inquirer questions and options. Need sections for Title, Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
@@ -58,17 +61,17 @@ inquirer
             name:'title'
         },
         {
-            type:'input',
-            message: 'Please describe the project you have created.',
+            type:'editor',
+            message: 'Please describe the project you have created. Be sure to Exit and Save.',
             name: 'description'
         },
         {
-            type:'input',
+            type:'editor',
             message: 'What is the proper way to install your project?',
             name: 'installation'
         },
         {
-            type:'input',
+            type:'editor',
             message: 'What is the intended use of you program?',
             name:'usage'
         },
@@ -76,15 +79,16 @@ inquirer
             type:'list',
             message: 'What license is this project covered under? Use ARROW keys and ENTER to select.',
             name:'license',
-            choices:['MIT License','Apache License 2.0','BSD License','GPL License','Mozilla Public License 2.0','GNU General Public Lisence (GPL)','GNU Lesser General Public License (LPGL)']
+            choices:['MIT License','Apache License 2.0','BSD License','Mozilla Public License 2.0','GNU General Public Lisence (GPL)','GNU Lesser General Public License (LPGL)'],
+            default:'MIT License'
         },
         {
-            type:'input',
+            type:'editor',
             message:'Were there any other contributors?',
             name:'contributing'
         },
         {
-            type:'input',
+            type:'editor',
             message:'What kind of tests should a user run to make sure your program is running properly?',
             name:'tests'
         },
@@ -101,7 +105,23 @@ inquirer
     ])
     .then((response) => {
     // Call the README generating block
-    const readmeContent = generateReadMe(response.title, response.description, response.installation, response.usage, response.license, response.contributing, response.tests, response.github, response.questionEmail);
+    let licenseImage = "";
+
+    if (response.license === "MIT License"){
+        licenseImage ="[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if(response.license === "Apache License 2.0"){
+        licenseImage = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else if(response.license === "BSD License"){
+        licenseImage = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    } else if(response.license === "Mozilla Public License 2.0"){
+        licenseImage = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    } else if(response.license === "GNU General Public Lisence (GPL)"){
+        licenseImage = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    } else {
+        licenseImage = "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+    }
+
+    const readmeContent = generateReadMe(response.title, response.description, response.installation, response.usage, response.license, response.contributing, response.tests, response.github, response.questionEmail,licenseImage);
 
     fs.writeFile("README.md", readmeContent, 
         (err) => {
